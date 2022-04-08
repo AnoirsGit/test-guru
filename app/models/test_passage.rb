0d3695c513e82750b27302735e76d3808241a606
ranger <ranger@rangers-MacBook-Pro.local>
2022-04-08 12:49:05 +0600

    end of 9 lesson

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -6,6 +6,16 @@ class TestPassage < ApplicationRecord
   before_validation :before_validation_set_first_question, on: :create
   before_validation :before_validation_set_next_question, on: :update
 
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+    self.question = next_question
+    save
+  end
+
+  def completed?
+    question.nil?
+  end
+
   private 
 
   def before_validation_set_first_question
@@ -14,6 +24,19 @@ class TestPassage < ApplicationRecord
 
   def before_validation_set_next_question
     self.question = test.questions.order(:id).where('id > ?', self.question.id).first
+  end
+
+  def correct_answer?(answer_ids)
+    count_correct_answer = correct_answers.count
+    count_correct_answer == correct_answers.where(id: answer_ids).count && count_correct_answer == answer_ids.count
+  end
+
+  def correct_answers
+    question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', self.question.id).first
   end
 
 end
